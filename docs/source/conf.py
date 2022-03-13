4882b1c0b0eb7730411d54f37bf6e1aec1c77cb5
sweeight <eceip.enoz@gmail.com>
2022-03-13 17:23:46 +0900

    設定を更新した。

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -20,6 +20,8 @@
 project = 'smash-brothers-ultimate'
 copyright = '2022, H. S.'
 author = 'H. S.'
+version = "0.0.0"
+release = "0.0.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,6 +30,10 @@ author = 'H. S.'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    'recommonmark',
+    'sphinx_markdown_tables',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -43,7 +49,7 @@ language = 'ja'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ["_build", "Thumbs.db", "DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -57,3 +63,9 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+source_suffix = {".rst": "restructuredtext",
+                 ".md": "markdown",
+                 ".txt": "markdown",
+}
+
