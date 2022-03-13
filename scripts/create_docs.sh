90863df7eefa9c2caef9299f11a4ddfa820ec11c
sweeight <eceip.enoz@gmail.com>
2022-03-13 16:10:39 +0000

    不要なファイルを削除した。

--- scripts/create_docs.sh
+++ scripts/create_docs.sh
@@ -2,7 +2,7 @@ shell_script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo -e "\nBuild docs.==================================================="
 poetry run sphinx-build -b html ${shell_script_dir}/../docs/source ${shell_script_dir}/../docs/build
 
-echo -e "\nCopy docs.==================================================="
-cd ${shell_script_dir}/../docs/
-cp ./build/*.html ./
-cp ./build/.nojekyll ./
+# echo -e "\nCopy docs.==================================================="
+# cd ${shell_script_dir}/../docs/
+# cp ./build/*.html ./
+# cp ./build/.nojekyll ./
