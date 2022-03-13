15b40503945fd2b1a2e005344d0542606c9d47d6
sweeight <eceip.enoz@gmail.com>
2022-03-13 16:57:16 +0000

    htmlをサブモジュールで管理するようにした。

--- scripts/create_docs.sh
+++ scripts/create_docs.sh
@@ -2,7 +2,15 @@ shell_script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo -e "\nBuild docs.==================================================="
 poetry run sphinx-build -b html ${shell_script_dir}/../docs/source ${shell_script_dir}/../docs/build
 
-# echo -e "\nCopy docs.==================================================="
-# cd ${shell_script_dir}/../docs/
-# cp ./build/*.html ./
-# cp ./build/.nojekyll ./
+echo -e "\nPush docs.==================================================="
+echo "Do you want to push documents? [yes/no]"
+read input
+if [ $input = "yes" ] ; then
+    cd ${shell_script_dir}/../docs/build
+    git add .
+    git commit -m "update docs."
+    git push origin 
+else
+    echo "No push."
+fi
+
