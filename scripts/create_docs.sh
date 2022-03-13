4bc9c197989aa343ce621c56298f00c5542e8c8f
sweeight <eceip.enoz@gmail.com>
2022-03-13 15:38:45 +0000

    ドキュメントがpagesに表示されるように修正した。

--- scripts/create_docs.sh
+++ scripts/create_docs.sh
@@ -2,10 +2,7 @@ shell_script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo -e "\nBuild docs.==================================================="
 poetry run sphinx-build -b html ${shell_script_dir}/../docs/source ${shell_script_dir}/../docs/build
 
-cd ${shell_script_dir}/../docs/
-if [ ! -e html ]; then
-    mkdir html
-fi
 echo -e "\nCopy docs.==================================================="
-cp ./build/*.html ./html/
-cp ./build/.nojekyll ./html/
+cd ${shell_script_dir}/../docs/
+cp ./build/*.html ./
+cp ./build/.nojekyll ./
